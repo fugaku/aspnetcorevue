0ee80f7fb9bcceb19ede00e0d85c9ae9efd6e3fe
Daniel Sagita <daniel.sagita@gmail.com>
2019-07-09 20:45:17 +0700

    update view

--- src/ClientApp/src/App.vue
+++ src/ClientApp/src/App.vue
@@ -1,42 +1,33 @@
 <template>
   <div id="app" :style="{ background: 'blue', height: 'calc(100vh - 55px)' }">
-    <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout id="top-layout" style="min-height: 100vh">
       <a-layout-sider collapsible v-model="collapsed">
-        <div class="logo"/>
+        <div class="logo">AkuntingKU</div>>
         <a-menu theme="dark" :defaultSelectedKeys="['1']" mode="inline">
           <a-menu-item key="1">
-            <a-icon type="pie-chart"/>
-            <span>Option 1</span>
+            <a-icon type="home" />
+            <span>Home</span>
           </a-menu-item>
           <a-menu-item key="2">
-            <a-icon type="desktop"/>
-            <span>Option 2</span>
+            <a-icon type="profile" />
+            <span>Tipe Akun</span>
           </a-menu-item>
-          <a-sub-menu key="sub1">
-            <span slot="title">
-              <a-icon type="user"/>
-              <span>User</span>
-            </span>
-            <a-menu-item key="3">Tom</a-menu-item>
-            <a-menu-item key="4">Bill</a-menu-item>
-            <a-menu-item key="5">Alex</a-menu-item>
-          </a-sub-menu>
-          <a-sub-menu key="sub2">
-            <span slot="title">
-              <a-icon type="team"/>
-              <span>Team</span>
-            </span>
-            <a-menu-item key="6">Team 1</a-menu-item>
-            <a-menu-item key="8">Team 2</a-menu-item>
-          </a-sub-menu>
+          <a-menu-item key="3">
+            <a-icon type="bank" />
+            <span>Akun</span>
+          </a-menu-item>
+          <a-menu-item key="4">
+            <a-icon type="project" />
+            <span>Jurnal</span>
+          </a-menu-item>
           <a-menu-item key="9">
-            <a-icon type="file"/>
-            <span>File</span>
+            <a-icon type="setting" />
+            <span>Konfigurasi</span>
           </a-menu-item>
         </a-menu>
       </a-layout-sider>
       <a-layout>
-        <a-layout-header style="background: #fff; padding: 0"/>
+        <a-layout-header style="background: #fff; padding: 0" />
         <a-layout-content style="margin: 0 16px">
           <a-breadcrumb style="margin: 16px 0">
             <a-breadcrumb-item>User</a-breadcrumb-item>
@@ -44,19 +35,19 @@
           </a-breadcrumb>
           <div :style="{ padding: '24px', background: '#fff', minHeight: '360px' }">Bill is a cat.</div>
         </a-layout-content>
-        <a-layout-footer style="text-align: center">Ant Design ©2018 Created by Ant UED</a-layout-footer>
+        <a-layout-footer style="text-align: center">AkuntingKU © 2019</a-layout-footer>
       </a-layout>
     </a-layout>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "./components/HelloWorld.vue";
 
 @Component({
   components: {
-    HelloWorld,
+    HelloWorld
   }
 })
 export default class App extends Vue {
@@ -69,11 +60,11 @@ export default class App extends Vue {
 </script>
 
 <style>
-#components-layout-demo-top-side-2 .logo {
-  width: 120px;
-  height: 31px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px 28px 16px 0;
-  float: left;
+#top-layout .logo {
+  height: 32px;
+  margin: 16px;
+  font-size: 20px;
+  color: white;
+  text-align: center;
 }
 </style>
